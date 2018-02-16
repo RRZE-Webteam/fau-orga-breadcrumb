fe400131df79aa0fd1cd2d458b98738d25df0f45
Wolfgang Wiese <xwolf@xwolf.de>
2018-02-16 18:30:17 +0100

    nun mit selectlist

--- includes/settings.php
+++ includes/settings.php
@@ -29,25 +29,52 @@ function fau_orga_breadcrumb_option_page(){?>
 function fau_orga_breadcrumb_settings() {
     register_setting( 'fau_orga_breadcrumb_options', 'fau_orga_breadcrumb_options' );
     add_settings_section('plugin_main', 'FAU.ORG Breadcrumb Einstellungen', 'FAU\ORGA\Breadcrumb\fau_orga_breadcrumb_section_text', 'fau_orga_textfield');
-    add_settings_field('Checkbox Element', 'FAU.ORG Id', 'FAU\ORGA\Breadcrumb\fau_orga_breadcrumb_field_callback', 'fau_orga_textfield', 'plugin_main' );
+    add_settings_field('Checkbox Element', 'Einrichtung', 'FAU\ORGA\Breadcrumb\fau_orga_breadcrumb_field_callback', 'fau_orga_textfield', 'plugin_main' );
 
 }
 
 function fau_orga_breadcrumb_field_callback() {
+    
+    global $fau_orga_breadcrumb_data;
     $options = get_option( 'fau_orga_breadcrumb_options' );
     if (isset($options['site-orga'])) {
 	 $orga = esc_attr($options['site-orga']);
     } else {
 	$orga = '0000000000';
     }
-   
-    echo '<input type="text" name="fau_orga_breadcrumb_options[site-orga]" value="'.$orga.'">';
+    ?>
+     <select id="fau_orga_breadcrumb_options[site-orga]"
+        name="fau_orga_breadcrumb_options[site-orga]" type="text">
+            <option value=""><?php _e('Keine (Keine Fakulätszuordnung oder Zentralbereich)', 'fau-orga-breadcrumb' ) ?></option>
+        <?php        
+	
+	$startorg = '000000000';
+	$firstlevel = get_fau_orga_childs('000000000');
+	if (!empty($firstlevel)) {
+	    foreach($firstlevel as $key) {
+		
+		echo '<option value="'.$key.'" '.selected( $orga, $sub ).'>'.$fau_orga_breadcrumb_data[$key]['title'].'</option>';
+		
+		$sublist = get_fau_orga_childs($key);
+		if (!empty($sublist)) {
+		    echo '<optgroup label="'.__('Untergeordnete Einrichtungen:','fau-orga-breadcrumb').'">';
+			 foreach($sublist as $sub) {
+			     echo '<option value="'.$sub.'" '.selected( $orga, $sub ).'>'.$fau_orga_breadcrumb_data[$sub]['title'].'</option>';
+			 }
+		    echo '</optgroup>';
+		}
+	
+	    }
+	}
+        ?>
+        </select>
+	<?php 
+ 
 }
-?>
-<?php 
+
 
 function fau_orga_breadcrumb_section_text() {
-    echo '<p>' . esc_html_e('Organisatorische Zuordnung. Bitte geben Sie hier die FAU.ORG Id der zur Website nächsthöheren Organisationseinheit an.','fau-orga-breadcrumb') . '</p>';
+    echo '<p>' . esc_html_e('Organisatorische Zuordnung. Bitte wählen Sie hier die nächsthöhere Organisationseinheit aus.','fau-orga-breadcrumb') . '</p>';
 }
 
 
