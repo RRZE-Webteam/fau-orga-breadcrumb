282e027af78a240c16952ed9a1e4113bdcd2b3ea
Rolf <rolf.v.d.forst@fau.de>
2025-09-11 17:42:00 +0200

    Add build files

---
+++ build/admin.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'edbe5d889e3316493e99');
---
+++ build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5c11b5782b6c73e62d94');
