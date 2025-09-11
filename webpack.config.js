83498b2dffeaf9d4aca108b4483613147ea5056a
Rolf <rolf.v.d.forst@fau.de>
2025-09-11 17:43:30 +0200

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+const defaults = require("@wordpress/scripts/config/webpack.config");
+
+/**
+ * WP-Scripts Webpack config.
+ *
+ * @see https://developer.wordpress.org/block-editor/packages/packages-scripts/#provide-your-own-webpack-config
+ */
+module.exports = {
+    ...defaults,
+    entry: {
+        admin: "./src/admin/index.js",
+        frontend: "./src/frontend/index.js",
+    },
+};
