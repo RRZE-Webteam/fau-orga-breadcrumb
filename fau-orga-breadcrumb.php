ac24ecb21d0e5a5c96a834739aeeaa7555ffe4c9
Regina Steinberg <regina.steinberg@fau.de>
2026-03-02 08:56:45 +0100

    fix: version number

--- fau-orga-breadcrumb.php
+++ fau-orga-breadcrumb.php
@@ -3,7 +3,7 @@
 /*
 Plugin Name:        FAU ORGA Breadcrumb
 Plugin URI:         https://github.com/RRZE-Webteam/fau-orga-breadcrumb
-Version:            1.2.5
+Version:            1.2.6
 Description:        Displays an organizational breadcrumb
 Author:             RRZE Webteam
 Author URI:         https://www.wp.rrze.fau.de/
